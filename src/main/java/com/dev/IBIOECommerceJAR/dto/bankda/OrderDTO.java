c511b24b39ec41eb7159b444c9fc67de72257cae
hwaaaankim <thramania@hanmail.net>
2024-07-11 15:38:04 +0900

    bankda

--- src/main/java/com/dev/IBIOECommerceJAR/dto/bankda/OrderDTO.java
+++ src/main/java/com/dev/IBIOECommerceJAR/dto/bankda/OrderDTO.java
@@ -1,5 +1,6 @@
 package com.dev.IBIOECommerceJAR.dto.bankda;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -10,13 +11,15 @@ public class OrderDTO {
 
 	private Long order_id;
 	private String buyer_name;
-	private String buyer_email;
-	private String buyer_cellphone;
 	private String billing_name;
 	private String bank_account_no;
 	private String bank_code_name;
-	private String order_date;
-	private String order_price_amount; 
+	private Date order_date;
+	private Integer order_price_amount; 
+	
+	
+	private String buyer_email;
+	private String buyer_cellphone;
 	private List<Map<String, Object>> item;
 	private String product_name;
 }
